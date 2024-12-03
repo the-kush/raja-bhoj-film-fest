e59a428b14ecebf458ce73afea84a19ddff9dcda
Kush <kushsinghdsc@gmail.com>
2024-12-03 14:21:35 +0530

    added few details about register

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState, useRef} from 'react'
+
+const TiltCard = ({ children, className}) => {
+    const [transformStyle, setTransformStyle] = useState('')
+    const itemRef = useRef();
+
+    const handleMouseMove = (e) => {
+        if(!itemRef.current) return;
+
+        const { left, top, width, height } = itemRef.current.getBoundingClientRect();
+
+        const relativeX = (e.clientX - left)/ width;
+        const relativeY = (e.clientY - top) / height;
+
+        const tiltX = (relativeY - 0.5) * 25;
+        const tiltY = (relativeX - 0.5) * -25;
+
+        const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95, 0.95, 0.95)`;
+
+
+
+        setTransformStyle(newTransform)
+    }
+
+    const handleMouseLeave = () => {
+        setTransformStyle('')
+    }
+
+    return (
+        <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ transform: transformStyle }}>
+            {children}
+        </div>
+    )
+}
 
 const Registration = () => {
     return (
-        <div>Registration</div>
+        <section id="register" className="pb-52">
+            <div className="container mx-auto px-3 md:px-19">
+                <div className='px-5 py-32 flex flex-col items-center justify-center'>
+                    <p className='font-circular-web text-lg text-blue-50'>Raja Bhoj Film Festival
+                    </p>
+                    <p className='max-w-md text-blue-50 opacity-50 font-circular-web text-lg'>Unveiling the Glory of Ancient Hindu Kingdoms
+                    </p>
+                    <p className=" text-blue-50 opacity-50 font-circular-web text-lg">Step into an era of unparalleled grandeur, where legends and history come alive through the magic of cinema. The Raja Bhoj Film Festival invites you to experience a cinematic journey through the world of ancient Hindu kingdoms. Hosted first in the heart of heritage-rich Bhopal, and then in the bustling film capital, Mumbai, this festival pays tribute to India’s glorious past, inspiring an exploration of our rich cultural legacy and untold stories.</p>
+                </div>
+
+                <TiltCard className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                    <div>
+                        <video
+                            src="/videos/registration-1.mp4"
+                            loop
+                            muted
+                            autoPlay
+                            className="absolute size-full object-cover object-center"/>
+                        <div className="relative z-20 text-blue-50 flex flex-col justify-between p-5">
+                            <div className="flex flex-col items-center justify-center text-black pt-14 mt-4">
+                                <h1 className="bento-title special-font">Register</h1>
+                                <p className="mt-3 text-xm md:text-base">Click to Learn more or Register for the event</p>
+                            </div>
+                        </div>
+                    </div>
+                </TiltCard>
+            </div>
+        </section>
     )
 }
 export default Registration
